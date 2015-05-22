9cfb8c93f19c98d1964d54d555ff6453e5ff800a
Ukraa <ukraa@outlook.com>
2015-05-22 13:53:55 +0200

    Random message
    Added some random messages when the numbers are too low or to high.

--- game.py
+++ game.py
@@ -11,20 +11,28 @@ print('Well, ' + myName + ', I am thinking of a number between 1 and 100.')
 
 while True:
 
-    print('Take a guess.You have ' + str(guessesTaken) + ' tries')
+    if guessesTaken == 1:
+        print("This is your last chance")
+    else:
+        print('You have ' + str(guessesTaken) + ' tries')
+
     guess = input()
     guess = int(guess)
 
+    list =["HAHA!","You will never do it!","Just stop it!","Its just a number how hard can it be?","Can you count untill 5?"]
+    index=random.randint(0,4)
     if guess < number:
-        print('Your guess is too low.')
+        print('Your guess is too low. ' + list[index])
 
     if guess > number:
-        print('Your guess is too high.')
+        print('Your guess is too high.' + list[index])
 
     if guess == number:
         print('Congratulations!you are smarter than you think!')
         break
+
+
+    guessesTaken -= 1
     if guessesTaken == 0:
         print("Idiot,cant even guess a number.Whar are you doing with your life?")
         break
-    guessesTaken -= 1

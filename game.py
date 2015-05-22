065502ef048b96e7c030c4273626a32c1e9f125b
Ukraa <ukraa@outlook.com>
2015-05-22 14:12:28 +0200

    Refactoring
    Defined as two functions.

--- game.py
+++ game.py
@@ -1,38 +1,43 @@
 # Guess the number
 
 import random
+def main():
+    guessesTaken = 10
+    print('Hello! What is your name?')
+    myName = input()
 
-guessesTaken = 10
-print('Hello! What is your name?')
-myName = input()
+    number = random.randint(1, 100)
+    print('Well, ' + myName + ', I am thinking of a number between 1 and 100.')
 
-number = random.randint(1, 100)
-print('Well, ' + myName + ', I am thinking of a number between 1 and 100.')
+    while True:
 
-while True:
+        if guessesTaken == 1:
+            print("This is your last chance")
+        else:
+            print('You have ' + str(guessesTaken) + ' tries')
 
-    if guessesTaken == 1:
-        print("This is your last chance")
-    else:
-        print('You have ' + str(guessesTaken) + ' tries')
+        guess = input()
+        guess = int(guess)
 
-    guess = input()
-    guess = int(guess)
 
+        if guess < number:
+            print('Your guess is too low. ' + get_message())
+
+        if guess > number:
+            print('Your guess is too high.' + get_message())
+
+        if guess == number:
+            print('Congratulations!you are smarter than you think!')
+            break
+
+
+        guessesTaken -= 1
+        if guessesTaken == 0:
+            print("Idiot,cant even guess a number.Whar are you doing with your life?")
+            break
+
+def get_message():
     list =["HAHA!","You will never do it!","Just stop it!","Its just a number how hard can it be?","Can you count untill 5?"]
     index=random.randint(0,4)
-    if guess < number:
-        print('Your guess is too low. ' + list[index])
-
-    if guess > number:
-        print('Your guess is too high.' + list[index])
-
-    if guess == number:
-        print('Congratulations!you are smarter than you think!')
-        break
-
-
-    guessesTaken -= 1
-    if guessesTaken == 0:
-        print("Idiot,cant even guess a number.Whar are you doing with your life?")
-        break
+    return list[index]
+main()

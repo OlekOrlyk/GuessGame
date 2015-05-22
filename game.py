a337412b3d32b6d5237cf3c4f2bb6d191103dc77
Ukraa <ukraa@outlook.com>
2015-05-22 13:19:58 +0200

    formating
    just test

--- game.py
+++ game.py
@@ -1,33 +1,30 @@
-#Guess the number 
+# Guess the number
 
 import random
 
-guessesTaken = 10 
+guessesTaken = 10
 print('Hello! What is your name?')
 myName = input()
 
 number = random.randint(1, 100)
 print('Well, ' + myName + ', I am thinking of a number between 1 and 100.')
 
-while True :
-    
-    print('Take a guess.You have '  +  str(guessesTaken)  + ' tries' )     
+while True:
+
+    print('Take a guess.You have ' + str(guessesTaken) + ' tries')
     guess = input()
     guess = int(guess)
-    
 
     if guess < number:
-         print('Your guess is too low.') 
-        
+        print('Your guess is too low.')
+
     if guess > number:
         print('Your guess is too high.')
 
     if guess == number:
         print('Congratulations!you are smarter than you think!')
         break
-    if guessesTaken==0:
+    if guessesTaken == 0:
         print("Idiot,cant even guess a number.Whar are you doing with your life?")
         break
-    guessesTaken-=1
-
-    
+    guessesTaken -= 1
